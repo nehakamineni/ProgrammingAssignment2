5fccdf18ce11e7376941825a7428bc8662892a58
nehakamineni <kaminenineha91@gmail.com>
2014-08-24 17:54:32 -0400

    R programming assignment 2
    R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a matrix object 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_xmat = NULL
+  
+  #set matrix function
+  set <- function(y) {
+    x <<- y
+    inv_xmat <<- NULL       ##initializing inverse matrix to null
+  }
+  
+  #Get Matrix function
+  get <- function() x
+  
+  #Set inverse of matrix function
+  setinverse<- function(inverse) inv_xmat <<-inverse
+  
+  #Get Inverse of matrix function
+  getinverse <- function() inv_xmat
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## The function cacheSolve returns inverse of matrix
+## If inverse is avaialbe in cache, cacheSolve function retreives else it
+## computes it and sets the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  
+  ## Returns the inverse of matrix x 
+  xinv = x$getinverse()
+  
+  #Checks if cache has any value
+  if (!is.null(xinv)) {
+    message("Displaying cached inverse of the matrix")
+    return(xinv)
+  } 
+  else {
+    xinv = solve(x$get())
+    x$setinverse(xinv)
+    return(xinv)
+  }
 }
+
+# t=makeCacheMatrix(matrix(rnorm(10000),100,100))
+
+# cacheSolve(t)
+
+# p=solve(matrix(rnorm(10000),100,100))
+
